949a1e81df3801c557f4b0827a24f53cb172d2c7
Raoul v. R <vanruesc@outlook.de>
2016-10-10 18:33:08 +0200

    Updated library index.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,39 @@
 /**
  * Exposure of the library components.
  *
- * @module rabbithole
+ * @module rabbit-hole
+ * @main rabbit-hole
  */
 
+export {
+	Terrain,
+	Queue,
+	PriorityQueue,
+	IteratorResult
+} from "./core";
+
+export { TerrainMaterial } from "./materials";
+
+export {
+	SingularValueDecomposition,
+	Givens,
+	Schur,
+	SymmetricMatrix3,
+	Matrix3,
+	Vector3,
+	QEFSolver,
+	QEFData
+} from "./math";
+
+export {
+	SignedDistanceFunction,
+	Heightfield,
+	Sphere,
+	Torus,
+	Plane,
+	Box,
+	ConstructiveSolidGeometry,
+	Intersection,
+	Difference,
+	Union
+} from "./volume";
