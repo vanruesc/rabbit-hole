149933a64ee978aee98e7f0d3bf02bac747007da
Raoul v. R <vanruesc@outlook.de>
2016-11-24 01:00:10 +0100

    Added asset preloading.

--- editor/index.js
+++ editor/index.js
@@ -1,3 +1,10 @@
+import {
+	// CubeTextureLoader,
+	LoadingManager,
+	RepeatWrapping,
+	TextureLoader
+} from "three";
+
 import { Editor } from "./editor.js";
 
 /**
@@ -20,9 +27,52 @@ window.addEventListener("load", function main(event) {
 
 	window.removeEventListener("load", main);
 
-	Editor.initialise(
-		document.getElementById("viewport"),
-		document.getElementById("aside")
-	);
+	const assets = new Map();
+
+	const loadingManager = new LoadingManager();
+	const textureLoader = new TextureLoader(loadingManager);
+	/* const cubeTextureLoader = new CubeTextureLoader(loadingManager);
+
+	const path = "textures/skies/sunny/";
+	const format = ".png";
+	const urls = [
+		path + "px" + format, path + "nx" + format,
+		path + "py" + format, path + "ny" + format,
+		path + "pz" + format, path + "nz" + format
+	]; */
+
+	loadingManager.onProgress = function onProgress(item, loaded, total) {
+
+		if(loaded === total) {
+
+			Editor.initialise(
+				document.getElementById("viewport"),
+				document.getElementById("aside"),
+				assets
+			);
+
+		}
+
+	};
+
+	// cubeTextureLoader.load(urls, function(textureCube) {
+
+		// assets.set("sky", textureCube);
+
+	// });
+
+	textureLoader.load("textures/tiles-diffuse.png", function(texture) {
+
+		texture.wrapS = texture.wrapT = RepeatWrapping;
+		assets.set("tiles-diffuse", texture);
+
+	});
+
+	textureLoader.load("textures/tiles-normalmap.png", function(texture) {
+
+		texture.wrapS = texture.wrapT = RepeatWrapping;
+		assets.set("tiles-normalmap", texture);
+
+	});
 
 });
