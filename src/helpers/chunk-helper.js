2d9ad697017fa08583276b0d38af9972db101af8
Raoul v. R <vanruesc@outlook.de>
2016-12-04 16:30:38 +0100

    Fixed array length of material index positions.

--- src/helpers/chunk-helper.js
+++ src/helpers/chunk-helper.js
@@ -140,7 +140,7 @@ export class ChunkHelper extends Object3D {
 
 		const geometry = new BufferGeometry();
 
-		const vertexCount = Math.pow((n + 1), 3);
+		const vertexCount = chunk.data.materials;
 		const positions = new Float32Array(vertexCount * 3);
 		const colors = new Float32Array(vertexCount * 3);
 
