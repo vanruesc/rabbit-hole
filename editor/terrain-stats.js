d766acc00f6bbb01a4809455e9f36a9b12b1f624
Raoul v. R <vanruesc@outlook.de>
2016-12-04 16:40:47 +0100

    Code cleanup.

--- editor/terrain-stats.js
+++ editor/terrain-stats.js
@@ -4,95 +4,23 @@ import Stats from "stats.js";
  * A terrain stats monitor.
  *
  * @class TerrainStats
- * @extends Stats
+ * @implements EventListener
  * @constructor
  * @param {Terrain} terrain - A terrain instance.
  */
 
-export class TerrainStats extends Stats {
+export class TerrainStats {
 
 	constructor(terrain) {
 
-		super();
-
-		this.dom.id = "stats";
-
 		/**
-		 * Additional panels.
+		 * A stats monitor.
 		 *
-		 * @property panels
-		 * @type Map
-		 * @private
+		 * @property stats
+		 * @type Stats
 		 */
 
-		const panels = {
-			modificationend: this.addPanel(new Stats.Panel("CSG", "#ff8", "#221")),
-			extractionend: this.addPanel(new Stats.Panel("DC", "#f8f", "#212"))
-		};
-
-		this.showPanel(3);
-
-		/**
-		 * Measured execution delta times.
-		 *
-		 * @property deltas
-		 * @type Map
-		 * @private
-		 */
-
-		const deltas = new Map();
-
-		/**
-		 * Measured delta time maxima.
-		 *
-		 * @property maxDeltas
-		 * @type Map
-		 * @private
-		 */
-
-		const maxDeltas = {
-			modificationend: 0.0,
-			extractionend: 0.0
-		};
-
-		/**
-		 * Handles modification and extraction events.
-		 *
-		 * @method onStart
-		 * @private
-		 * @param {Event} event - An event.
-		 */
-
-		this.onStart = function(event) {
-
-			deltas.set(event.chunk, performance.now());
-
-		};
-
-		/**
-		 * Handles modification and extraction events.
-		 *
-		 * @method onEnd
-		 * @private
-		 * @param {Event} event - An event.
-		 */
-
-		this.onEnd = function(event) {
-
-			const delta = performance.now() - deltas.get(event.chunk);
-			const maxDelta = maxDeltas[event.type];
-
-			panels[event.type].update(delta, maxDelta);
-
-			deltas.delete(event.chunk);
-
-			if(delta > maxDelta) {
-
-				maxDeltas[event.type] = delta;
-
-			}
-
-		};
+		this.stats = new Stats();
 
 		/**
 		 * The terrain.
@@ -104,28 +32,141 @@ export class TerrainStats extends Stats {
 
 		this.terrain = terrain;
 
-		terrain.addEventListener("modificationstart", this.onStart);
-		terrain.addEventListener("extractionstart", this.onStart);
-		terrain.addEventListener("modificationend", this.onEnd);
-		terrain.addEventListener("extractionend", this.onEnd);
+		/**
+		 * A panel for volume modifications.
+		 *
+		 * @property modificationPanel
+		 * @type Panel
+		 * @private
+		 */
+
+		this.modificationPanel = this.stats.addPanel(new Stats.Panel("CSG", "#ff8", "#221"));
+
+		/**
+		 * A panel for surface extractions.
+		 *
+		 * @property extractionPanel
+		 * @type Panel
+		 * @private
+		 */
+
+		this.extractionPanel = this.stats.addPanel(new Stats.Panel("DC", "#f8f", "#212"));
+
+		/**
+		 * Measured delta times.
+		 *
+		 * @property deltas
+		 * @type Map
+		 * @private
+		 */
+
+		this.deltas = new Map();
+
+		/**
+		 * Measured delta time maxima.
+		 *
+		 * @property maxDeltas
+		 * @type Map
+		 * @private
+		 */
+
+		this.maxDeltas = new Map();
+
+		this.setEnabled(true);
 
 	}
 
 	/**
-	 * Destroys this stats monitor.
+	 * Handles terrain events.
+	 *
+	 * @method handleEvent
+	 * @param {TerrainEvent} event - A terrain event.
+	 */
+
+	handleEvent(event) {
+
+		switch(event.type) {
+
+			case "extractionstart":
+			case "modificationstart":
+				this.deltas.set(event.chunk, performance.now());
+				break;
+
+			case "modificationend":
+				this.measureTime(event, this.modificationPanel);
+				break;
+
+			case "extractionend":
+				this.measureTime(event, this.extractionPanel);
+				break;
+
+		}
+
+	}
+
+	/**
+	 * Measures execution time.
+	 *
+	 * @method measureTime
+	 * @private
+	 * @param {TerrainEvent} event - A terrain event.
+	 * @param {Panel} panel - The panel to update.
+	 */
+
+	measureTime(event, panel) {
+
+		const maxDeltas = this.maxDeltas;
+		const deltas = this.deltas;
+
+		const delta = performance.now() - deltas.get(event.chunk);
+		const maxDelta = maxDeltas.has(event.type) ? maxDeltas.get(event.type) : 0.0;
+
+		if(delta > maxDelta) {
+
+			maxDeltas.set(event.type, delta);
+
+		}
+
+		panel.update(delta, maxDelta);
+		deltas.delete(event.chunk);
+
+	}
+
+	/**
+	 * Enables or disables this stats monitor.
+	 *
+	 * @method setEnabled
+	 * @param {Boolean} enabled - Whether this monitor should be enabled or disabled.
+	 */
+
+	setEnabled(enabled) {
+
+		const terrain = this.terrain;
+
+		if(enabled) {
+
+			terrain.addEventListener("modificationstart", this);
+			terrain.addEventListener("extractionstart", this);
+			terrain.addEventListener("modificationend", this);
+			terrain.addEventListener("extractionend", this);
+
+		} else {
+
+			terrain.removeEventListener("modificationstart", this);
+			terrain.removeEventListener("extractionstart", this);
+			terrain.removeEventListener("modificationend", this);
+			terrain.removeEventListener("extractionend", this);
+
+		}
+
+	}
+
+	/**
+	 * Removes all event listeners.
 	 *
 	 * @method dispose
 	 */
 
-	dispose() {
-
-		const terrain = this.terrain;
-
-		terrain.removeEventListener("modificationstart", this.onStart);
-		terrain.removeEventListener("extractionstart", this.onStart);
-		terrain.removeEventListener("modificationend", this.onEnd);
-		terrain.removeEventListener("extractionend", this.onEnd);
-
-	}
+	dispose() { this.setEnabled(false); }
 
 }
