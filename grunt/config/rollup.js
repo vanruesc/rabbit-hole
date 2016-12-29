da7aed18e96fd278b61be5b640462e973fa7512e
Raoul v. R <vanruesc@outlook.de>
2016-12-29 13:32:10 +0100

    Added production switch for bundling with babel.

--- grunt/config/rollup.js
+++ grunt/config/rollup.js
@@ -2,58 +2,75 @@ const resolve = require("rollup-plugin-node-resolve");
 const string = require("rollup-plugin-string");
 const babel = require("rollup-plugin-babel");
 
-module.exports = {
+module.exports = function(grunt) {
 
-	options: {
-		globals: {
-			"three": "THREE",
-			"stats.js": "Stats",
-			"dat.gui": "dat"
-		},
-		external: [
-			"three",
-			"stats.js",
-			"dat.gui"
-		],
-		plugins: () => [
-			resolve({
-				jsnext: true
-			}),
-			string({
-				include: [
-					"**/*.frag",
-					"**/*.vert",
-					"**/*.tmp"
-				]
-			}),
-			babel()
-		]
-	},
+	return {
 
-	worker: {
 		options: {
-			format: "iife"
+			globals: {
+				"three": "THREE",
+				"stats.js": "Stats",
+				"dat.gui": "dat"
+			},
+			external: [
+				"three",
+				"stats.js",
+				"dat.gui"
+			],
+			plugins() {
+				return grunt.option("production") ? [
+					resolve({
+						jsnext: true
+					}),
+					string({
+						include: [
+							"**/*.frag",
+							"**/*.vert",
+							"**/*.tmp"
+						]
+					}),
+					babel()
+				] : [
+					resolve({
+						jsnext: true
+					}),
+					string({
+						include: [
+							"**/*.frag",
+							"**/*.vert",
+							"**/*.tmp"
+						]
+					})
+				];
+			}
 		},
-		src: "src/worker/worker.js",
-		dest: "src/worker/worker.tmp"
-	},
 
-	lib: {
-		options: {
-			format: "umd",
-			moduleName: "<%= package.name.replace(/-/g, \"\").toUpperCase() %>",
-			banner: "<%= banner %>"
+		worker: {
+			options: {
+				format: "iife"
+			},
+			src: "src/worker/worker.js",
+			dest: "src/worker/worker.tmp"
 		},
-		src: "src/index.js",
-		dest: "build/<%= package.name %>.js"
-	},
 
-	editor: {
-		options: {
-			format: "iife"
+		lib: {
+			options: {
+				format: "umd",
+				moduleName: "<%= package.name.replace(/-/g, \"\").toUpperCase() %>",
+				banner: "<%= banner %>"
+			},
+			src: "src/index.js",
+			dest: "build/<%= package.name %>.js"
 		},
-		src: "editor/index.js",
-		dest: "public/index.js"
-	}
+
+		editor: {
+			options: {
+				format: "iife"
+			},
+			src: "editor/index.js",
+			dest: "public/index.js"
+		}
+
+	};
 
 };
