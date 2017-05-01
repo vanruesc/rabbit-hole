4e5175c2946f96bf3204c1f838d642f33555dbd5
Raoul v. R <vanruesc@outlook.de>
2017-05-01 18:32:53 +0200

    Re-Export Vector2.

--- src/math/index.js
+++ src/math/index.js
@@ -12,6 +12,7 @@ export { Schur } from "./schur.js";
 export { SymmetricMatrix3 } from "./symmetric-matrix3.js";
 export { Matrix3 } from "./matrix3.js";
 export { Box3 } from "./box3.js";
+export { Vector2 } from "./vector2.js";
 export { Vector3 } from "./vector3.js";
 
 export { QEFSolver } from "./qef-solver.js";
