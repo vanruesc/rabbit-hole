a84fa0e592ce853518c794d73a0e26a599d80bd2
Raoul v. R <vanruesc@outlook.de>
2017-08-30 03:01:21 +0200

    Added DataProcessor base class.

---
+++ src/worker/DataProcessor.js
@@ -0,0 +1,50 @@
+import { HermiteData } from "../volume/HermiteData.js";
+
+/**
+ * A volume data processor.
+ */
+
+export class DataProcessor {
+
+	/**
+	 * Constructs a new data processor.
+	 */
+
+	constructor() {
+
+		/**
+		 * An empty set of Hermite data. Serves as a tool for the deserialization of
+		 * volume data received from the main thread.
+		 *
+		 * @type {HermiteData}
+		 */
+
+		this.data = new HermiteData(false);
+
+		/**
+		 * A container for the data that will be returned to the main thread.
+		 *
+		 * @type {Response}
+		 */
+
+		this.response = null;
+
+		/**
+		 * A list of transferable objects.
+		 *
+		 * @type {ArrayBuffer[]}
+		 */
+
+		this.transferList = null;
+
+	}
+
+	/**
+	 * Processes the given request.
+	 *
+	 * @param {Request} request - A request.
+	 */
+
+	process(request) {}
+
+}
