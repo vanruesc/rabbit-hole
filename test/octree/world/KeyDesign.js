26832acc17daaa21124f596e6a2267bf3d9ebd73
Raoul v. R <vanruesc@outlook.de>
2017-10-17 15:49:46 +0200

    Added key range iterator test.
    Moved KeyDesign test to test/octree/world.

--- test/octree/world/KeyDesign.js
+++ test/octree/world/KeyDesign.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const KeyDesign = require("../../build/rabbit-hole").KeyDesign;
+const KeyDesign = require("../../../build/rabbit-hole").KeyDesign;
 const Vector3 = require("math-ds").Vector3;
 
 module.exports = {
@@ -23,8 +23,29 @@ module.exports = {
 			const position = new Vector3(11, 22, 33);
 			const key = keyDesign.packKey(position);
 
-			test.ok(key >= 0, "should be able to generate a key");
+			test.equal(key, 141780058123, "should be able to generate a correct key");
 			test.ok(position.equals(keyDesign.unpackKey(key)), "should be able to correctly unpack a key");
+			test.done();
+
+		},
+
+		"can iterate over a 3D key range": function(test) {
+
+			const keyDesign = new KeyDesign(21, 11, 21);
+
+			const min = new Vector3(0, 0, 0);
+			const max = new Vector3(1, 1, 1);
+			const iterator = keyDesign.keyRange(min, max);
+
+			let i = 0;
+
+			while(!iterator.next().done) {
+
+				++i;
+
+			}
+
+			test.equal(i, 8, "should return eight keys");
 			test.done();
 
 		}
