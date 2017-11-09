ace2d8c5e2aee18c6bf69ae9a6454a1c8c59c250
Raoul v. R <vanruesc@outlook.de>
2017-11-09 22:37:04 +0100

    Finished implementing Hermite data resampling.
    There may still be bugs. Have no way to debug right now.

--- src/volume/HermiteData.js
+++ src/volume/HermiteData.js
@@ -1,7 +1,7 @@
 import { pattern } from "sparse-octree";
 import { RunLengthEncoding } from "../compression/RunLengthEncoding.js";
-import { Material } from "./Material.js";
 import { EdgeData } from "./EdgeData.js";
+import { Material } from "./Material.js";
 
 /**
  * The material grid resolution.
@@ -38,16 +38,151 @@ function ceil2(n) {
 }
 
 /**
- * Resamples the given data.
+ * Resamples the given data and integrates it into a target set.
+ *
+ * Edges are preserved according to the following rules:
+ *
+ * ```text
+ * 0 = AIR
+ * 1 = SOLID
+ *
+ * 0   0   0   =>   0   0
+ * 1   1   1   =>   1   1
+ * 0---1---0   =>   0   0
+ * 1---0---1   =>   1   1
+ *
+ * 0   0---1   =>   0---1
+ * 1   1---0   =>   1---0
+ * 0---1   1   =>   0---1
+ * 1---0   0   =>   1---0
+ * ```
  *
  * @private
  * @param {HermiteData} data - A set of Hermite data.
- * @param {Uint8Array} offset - An offset.
+ * @param {Uint8Array} offset - A subsector position offset.
  * @param {Uint32Array} lengths - An edge counter.
- * @param {HermiteData} target - A target data set.
+ * @param {HermiteData} result - A target data set.
  */
 
-function resample(data, offset, lengths, target) {
+function resample(data, offset, lengths, result) {
+
+	const n = HermiteData.resolution;
+	const halfResolution = n >> 1;
+	const m = n + 1;
+	const mm = m * m;
+
+	const indexOffsets = new Uint32Array([1, m, mm]);
+
+	const materialIndices0 = data.materialIndices;
+	const materialIndices1 = result.materialIndices;
+
+	const edgeData0 = data.edgeData;
+	const edgeData1 = result.edgeData;
+
+	let edges0, zeroCrossings0, normals0;
+	let edges1, zeroCrossings1, normals1;
+	let indexOffset;
+
+	let indexA0, indexB0;
+	let indexA1, indexB1;
+	let m1, m2;
+
+	let c, d, i, l, t;
+	let x, y, z;
+
+	// Process the edges along the X-axis, then Y and finally Z.
+	for(c = 0, d = 0; d < 3; c = 0, ++d) {
+
+		edges0 = edgeData0.indices[d];
+		edges1 = edgeData1.indices[d];
+
+		zeroCrossings0 = edgeData0.zeroCrossings[d];
+		zeroCrossings1 = edgeData1.zeroCrossings[d];
+
+		normals0 = edgeData0.normals[d];
+		normals1 = edgeData1.normals[d];
+
+		indexOffset = indexOffsets[d];
+
+		// Process all existing edges of the current dimension.
+		for(i = 0, l = edges0.length; i < l; ++i) {
+
+			// The Zero Crossing applies to one half of the interval between A and B.
+			t = zeroCrossings0[i] * 0.5;
+
+			indexA0 = edges0[i];
+
+			if((indexA0 & 1) === 1) {
+
+				// Odd index. Snap back to the previous, even index.
+				--indexA0;
+
+				// The Zero Crossing is in the second half of the interval.
+				t += 0.5;
+
+			}
+
+			// Calculate the index of the material after the next one.
+			indexB0 = indexA0 + indexOffset * 2;
+
+			// Fetch the actual material indices.
+			m1 = materialIndices0[indexA0];
+			m2 = materialIndices0[indexB0];
+
+			// Check for a material change. If there is none, ignore the edge.
+			if(m1 !== m2) {
+
+				// Downsample.
+				indexA0 >>= 1;
+
+				// Calculate the local grid coordinates from the one-dimensional index.
+				x = indexA0 % m;
+				y = Math.trunc((indexA0 % mm) / m);
+				z = Math.trunc(indexA0 / mm);
+
+				// Apply the positional offset.
+				if(offset[0] > 0) {
+
+					x += halfResolution;
+
+				}
+
+				if(offset[1] > 0) {
+
+					y += halfResolution;
+
+				}
+
+				if(offset[2] > 0) {
+
+					z += halfResolution;
+
+				}
+
+				// Calculate the index into the target material array.
+				indexA1 = z * mm + y * m + x;
+				indexB1 = indexA1 + indexOffset;
+
+				// Save the material indices.
+				materialIndices1[indexA1] = m1;
+				materialIndices1[indexB1] = m2;
+
+				// Store the edge.
+				edges1[c] = indexA1;
+				zeroCrossings1[c] = t;
+				normals1[c * 3] = normals0[i * 3];
+				normals1[c * 3 + 1] = normals0[i * 3 + 1];
+				normals1[c * 3 + 2] = normals0[i * 3 + 2];
+
+				++c;
+
+			}
+
+		}
+
+		lengths[d] = c;
+
+	}
 
 }
 
