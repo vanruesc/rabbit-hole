bf05f24f21c330e11e985915ce8a5a5d091d3ee9
Raoul v. R <vanruesc@outlook.de>
2017-11-28 00:08:04 +0100

    Moved assets around.

--- grunt/tasks/build.js
+++ grunt/tasks/build.js
@@ -5,19 +5,19 @@ module.exports = function(grunt) {
 		"eslint",
 		"build:worker:min",
 		"build:lib:min",
-		"build:editor:min",
 		"build:demo:min",
+		"build:editor:min",
 		"clean:worker"
 	]);
 
 	grunt.registerTask("build:worker", ["eslint:lib", "rollup:worker"]);
 	grunt.registerTask("build:lib", ["eslint:lib", "rollup:lib"]);
-	grunt.registerTask("build:editor", ["eslint:editor", "rollup:editor"]);
 	grunt.registerTask("build:demo", ["eslint:demo", "rollup:demo"]);
+	grunt.registerTask("build:editor", ["eslint:editor", "rollup:editor"]);
 
 	grunt.registerTask("build:worker:min", ["rollup:worker", "uglify:worker"]);
 	grunt.registerTask("build:lib:min", ["rollup:lib", "uglify:lib"]);
-	grunt.registerTask("build:editor:min", ["rollup:editor", "uglify:editor"]);
 	grunt.registerTask("build:demo:min", ["rollup:demo", "uglify:demo"]);
+	grunt.registerTask("build:editor:min", ["rollup:editor", "uglify:editor"]);
 
 };
