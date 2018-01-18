9176f189e1577a1fda41aa10e4844366a00475ab
Raoul v. R <vanruesc@outlook.de>
2018-01-18 01:22:01 +0100

    Fixed Heightfield.

--- src/volume/sdf/Heightfield.js
+++ src/volume/sdf/Heightfield.js
@@ -1,4 +1,4 @@
-import { Box3, Vector3 } from "math-ds";
+import { Box3 } from "math-ds";
 import { SignedDistanceFunction } from "./SignedDistanceFunction.js";
 import { SDFType } from "./SDFType.js";
 
@@ -15,6 +15,8 @@ function readImageData(image) {
 	const canvas = document.createElementNS("http://www.w3.org/1999/xhtml", "canvas");
 	const context = canvas.getContext("2d");
 
+	canvas.width = image.width;
+	canvas.height = image.height;
 	context.drawImage(image, 0, 0);
 
 	return context.getImageData(0, 0, image.width, image.height);
@@ -31,7 +33,8 @@ export class Heightfield extends SignedDistanceFunction {
 	 * Constructs a new heightfield SDF.
 	 *
 	 * @param {Object} parameters - The parameters.
-	 * @param {Array} [parameters.size] - The size of the heightmap [x, y, z]. Y defines the maximum height.
+	 * @param {Array} [parameters.width] - The width of the heightfield.
+	 * @param {Array} [parameters.height] - The height of the heightfield.
 	 * @param {Uint8ClampedArray} [parameters.data] - The heightmap image data. Can be null.
 	 * @param {Image} [parameters.image] - The heightmap image.
 	 * @param {Number} [material] - A material index.
@@ -42,19 +45,22 @@ export class Heightfield extends SignedDistanceFunction {
 		super(SDFType.HEIGHTFIELD, material);
 
 		/**
-		 * The size.
+		 * The width.
 		 *
-		 * @type {Vector3}
+		 * @type {Number}
 		 * @private
 		 */
 
-		this.size = new Vector3(1, 1, 1);
+		this.width = (parameters.width !== undefined) ? parameters.width : 1;
 
-		if(parameters.size !== undefined) {
+		/**
+		 * The height.
+		 *
+		 * @type {Number}
+		 * @private
+		 */
 
-			this.size.fromArray(parameters.size);
-
-		}
+		this.height = (parameters.height !== undefined) ? parameters.height : 1;
 
 		/**
 		 * The height data.
@@ -63,7 +69,7 @@ export class Heightfield extends SignedDistanceFunction {
 		 * @private
 		 */
 
-		this.data = parameters.data;
+		this.data = (parameters.data !== undefined) ? parameters.data : null;
 
 		/**
 		 * The heightmap.
@@ -106,14 +112,15 @@ export class Heightfield extends SignedDistanceFunction {
 			// Reduce image data to greyscale format.
 			result = new Uint8ClampedArray(data.length / 4);
 
-			for(i = 0, j = 0, l = data.length; i < l; ++i, j += 4) {
+			for(i = 0, j = 0, l = result.length; i < l; ++i, j += 4) {
 
 				result[i] = data[j];
 
 			}
 
 			this.heightmap = image;
-			this.size.set(imageData.width, 1.0, imageData.height);
+			this.width = imageData.width;
+			this.height = imageData.height;
 			this.data = result;
 
 		}
@@ -132,8 +139,11 @@ export class Heightfield extends SignedDistanceFunction {
 
 		const boundingBox = new Box3();
 
-		boundingBox.min.set(-1, -1, -1);
-		boundingBox.max.set(1, 1, 1);
+		const w = Math.min(this.width / this.height, 1.0);
+		const h = Math.min(this.height / this.width, 1.0);
+
+		boundingBox.min.set(0, 0, 0);
+		boundingBox.max.set(w, 1, h);
 		boundingBox.applyMatrix4(this.getTransformation());
 
 		return boundingBox;
@@ -149,13 +159,30 @@ export class Heightfield extends SignedDistanceFunction {
 
 	sample(position) {
 
-		position.applyMatrix4(this.inverseTransformation);
+		const boundingBox = this.boundingBox;
 
-		const x = position.x;
-		const z = position.z;
-		const h = this.data[z * this.size.x + x] / 255;
+		let d;
 
-		return position.y - h;
+		if(boundingBox.containsPoint(position)) {
+
+			position.applyMatrix4(this.inverseTransformation);
+
+			const w = this.width;
+			const h = this.height;
+			const x = Math.max(Math.min(Math.trunc(position.x * w), w), 0);
+			const z = Math.max(Math.min(Math.trunc(position.z * h), h), 0);
+
+			const height = this.data[z * w + x] / 255;
+
+			d = position.y - height;
+
+		} else {
+
+			d = boundingBox.distanceToPoint(position);
+
+		}
+
+		return d;
 
 	}
 
@@ -171,7 +198,8 @@ export class Heightfield extends SignedDistanceFunction {
 		const result = super.serialize();
 
 		result.parameters = {
-			size: this.size.toArray(),
+			width: this.width,
+			height: this.height,
 			data: deflate ? null : this.data,
 			dataUrl: (deflate && this.heightmap !== null) ? this.heightmap.toDataUrl() : null,
 			image: null
